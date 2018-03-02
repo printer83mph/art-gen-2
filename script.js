4f7e919e6c51022f24dc0229649aff35ca43b1fb
Thomas Shaw <printer.83mph@gmail.com>
2018-03-01 20:14:39 -0500

    Update script

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 var canvas, ctx, minSize, indexPropMult;
 
-const props = ("angle", "length", "sides", "weight", "hue")
+const props = ["angle", "length", "sides", "weight", "hue"];
 // what each prop is based on
-var changeables = {};
-var vals = {};
+var vals = {}; // each list in this is one property
+var consts = {}; // constants
+var base = {}; // bases for each prop
 
 window.onload = function() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  minSize = min(canvas.width,canvas.height);
+  minSize = Math.min(canvas.width,canvas.height);
   // window setup
   setup();
   run();
 }
 
 function setup() {
-  if (Math.random() < 0.5) {
-    for (i in props) {
-      vals[i] = [];
-      let stat = Math.floor(Math.random()*4);
-      switch(stat) {
-        case 0:
-          // constant
-          break;
-        case 1:
-          // other prop
-          break;
-        case 2:
-          // sin wave
-          break;
-        default:
-          // linear increase, maybe alternate increase/decrease
-      }
+  indexPropMult = Math.random()*5;
+  for (i in props) {
+    console.log(props[i]);
+    let stat = Math.floor(Math.random()*4);
+    switch(stat) {
+      case 0:
+        // constant
+        consts[props[i]] = Math.random()*100;
+        break;
+      case 1:
+        // other prop of self
+        base[props[i]] = "P" + props[Math.floor(Math.random()*props.length)];
+        vals[props[i]] = [];
+        break;
+      case 2:
+        base[props[i]] = "S";
+        vals[props[i]] = [];
+        // sin waveprops
+        break;
+      default:
+        base[props[i]] = "linear";
+        vals[props[i]] = [];
+        // linear increase, maybe alternate increase/decrease
     }
     for (i = 0; i < Math.random() * minSize ** 2 / 10000; i++) {
 
@@ -42,5 +49,14 @@ function setup() {
 }
 
 function run() {
+  for (i in base) {
+    if (typeof base[props[i]] == "number") {
 
+    }
+  }
+  for (i in vals) {
+    if (i > 100) {
+
+    }
+  }
 }
