38337891d9bbc5da593ae96fb02f96599cc75877
Thomas Shaw <printer.83mph@gmail.com>
2018-03-02 14:33:46 -0500

    Add stuff

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function setup() {
   frameCount = 0;
   indexPropMult = Math.random()*5;
   for (i in props) {
-    console.log(props[i]);
     let stat = Math.floor(Math.random()*4);
     switch(stat) {
       case 0:
@@ -30,8 +29,13 @@ function setup() {
         break;
       case 1:
         // other prop of self
-        base[props[i]] = [Math.random()*10, props[Math.floor(Math.random()*props.length)]];
-        vals[props[i]] = [];
+        while (true) {
+          base[props[i]] = [props[Math.floor(Math.random()*props.length)], Math.random()*10];
+          vals[props[i]] = [];
+          if (base[props[i]][0] in base) {
+            if (!(base[base[props[i]][0]][0] === props[i])) { break; }
+          } else { break; }
+        }
         break;
       case 2:
         base[props[i]] = ["S", Math.random()*50, Math.random()*50];
@@ -39,41 +43,53 @@ function setup() {
         // sin waveprops
         break;
       default:
-        base[props[i]] = "linear";
+        base[props[i]] = Math.random()*10;
         vals[props[i]] = [];
         // linear increase, maybe alternate increase/decrease
     }
   }
   for (i = 0; i < Math.random() * minSize ** 2 / 10000; i++) {
     for (j in base) {
-      vals[j].push(getValFromBase(j,vals[j].length));
+      vals[j].push(newValFromBase(j,vals[j].length));
     }
   }
+}
+
+function newValFromBase(prop,index) {
+  if (typeof base[prop] === "number") {
+    // return old val plus base[prop]
+    return indexPropMult * index;
+  } if (base[prop][0] === "S") {
+    return Math.sin(frameCount/100)*base[prop][1]+base[prop][2]+indexPropMult*index;
+    // return sin of framecount times multiplier plus constant
+  }
+  // based on other prop
+  return getValFromBase(base[prop][0],index) + base[prop][1] + indexPropMult*index;
 }
 
 function getValFromBase(prop,index) {
   if (prop in consts) {
     return consts[prop];
-  } if (typeof base[prop] === "number") {
-    // return old val plus base[prop]
-  } if (base[prop][1] === "S") {
-    // return sin of framecount times multiplier plus constant
+  } if (prop in base) {
+    if (typeof base[prop] === "number") {
+      // return old val plus base[prop]
+      return vals[prop][index] + base[prop];
+    } if (base[prop][0] === "S") {
+      return Math.sin(frameCount/100)*base[prop][1]+base[prop][2]+indexPropMult*index;
+      // return sin of framecount times multiplier plus constant
+    }
+    // based on other prop
+    return getValFromBase(base[prop][0],index) + base[prop][1] + indexPropMult*index;
   }
-  return getValFromBase(base[prop][1],index) + base[prop][0];
 }
 
 function run() {
-  for (i in base) {
-    if (base[i] === "S") {
-      for (j in vals[i]) {
-
-      }
-    }
-  }
   for (i in vals) {
-    if (i > 100) {
-
+    for (j in vals[i]) {
+      vals[i][j] = getValFromBase(i,j);
+      vals[i][j] = vals[i][j] % 100;
     }
   }
   frameCount++;
+  window.requestAnimationFrame(run);
 }
