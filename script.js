3dc5d91bec3740d05f7bf608d1f4c3c573d46f9b
Thomas Shaw <printer.83mph@gmail.com>
2018-03-14 13:06:21 -0400

    Add stuff and break everything

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-var canvas, ctx, minSize, indexPropMult, frameCount;
+var canvas, ctx, minSize, indexPropMult, frameCount, vals, consts, base, len;
 
-const props = ["angle", "length", "sides", "weight", "hue"];
-// what each prop is based on
-var vals = {}; // each list in this is one property
-var consts = {}; // constants
-var base = {}; // bases for each prop
+const props = ["angle", "length", "sides", "weight", "hue", "saturation", "value", "x", "y"];
 
 window.onload = function() {
   canvas = document.getElementById("canvas");
@@ -14,10 +10,14 @@ window.onload = function() {
   minSize = Math.min(canvas.width,canvas.height);
   // window setup
   setup();
-  run();
+  window.requestAnimationFrame(run);
 }
 
 function setup() {
+  // what each prop is based on
+  vals = {}; // each list in this is one property
+  consts = {}; // constants
+  base = {}; // bases for each prop
   frameCount = 0;
   indexPropMult = Math.random()*5;
   for (i in props) {
@@ -25,6 +25,7 @@ function setup() {
     switch(stat) {
       case 0:
         // constant
+        vals[props[i]] = [];
         consts[props[i]] = Math.random()*100;
         break;
       case 1:
@@ -38,7 +39,7 @@ function setup() {
         }
         break;
       case 2:
-        base[props[i]] = ["S", Math.random()*50, Math.random()*50];
+        base[props[i]] = ["S", Math.random()*25, Math.random()*50];
         vals[props[i]] = [];
         // sin waveprops
         break;
@@ -48,9 +49,14 @@ function setup() {
         // linear increase, maybe alternate increase/decrease
     }
   }
-  for (i = 0; i < Math.random() * minSize ** 2 / 10000; i++) {
+  // len = Math.random() * minSize ** 2 / 10000;
+  len = 1;
+  for (i = 0; i < len; i++) {
     for (j in base) {
       vals[j].push(newValFromBase(j,vals[j].length));
+    }
+    for (j in consts) {
+      vals[j].push(consts[j]);
     }
   }
 }
@@ -75,7 +81,7 @@ function getValFromBase(prop,index) {
       // return old val plus base[prop]
       return vals[prop][index] + base[prop];
     } if (base[prop][0] === "S") {
-      return Math.sin(frameCount/100)*base[prop][1]+base[prop][2]+indexPropMult*index;
+      return (Math.sin(frameCount/100) + 1)*base[prop][1]+base[prop][2]+indexPropMult*index;
       // return sin of framecount times multiplier plus constant
     }
     // based on other prop
@@ -84,10 +90,26 @@ function getValFromBase(prop,index) {
 }
 
 function run() {
-  for (i in vals) {
+  ctx.fillRect(0,0,innerWidth,innerHeight);
+  console.log("vals length is " + vals["x"].length);
+  for (i in base) {
     for (j in vals[i]) {
       vals[i][j] = getValFromBase(i,j);
       vals[i][j] = vals[i][j] % 100;
+    }
+  }
+  for (var i = 0; i < len; i++) {
+    ctx.beginPath();
+    ctx.moveTo(vals["x"][i], vals["y"][i]);
+    var lastX = vals["x"][i];
+    var lastY = vals["y"][i];
+    for (var j = 0; j < vals["sides"][i] / 10; j++) {
+      ctx.strokeStyle = "hsl(" + vals["hue"][i]*3.6 + vals["saturation"][i] + vals["value"][i] + ")";
+      ctx.lineWidth = vals["weight"]/2;
+      var curAngle = vals["angle"][i] / 50 * Math.PI + Math.PI * 20 / j;
+      lastX += Math.cos(curAngle)*vals["length"][i];
+      lastY += Math.sin(curAngle)*vals["length"][i];
+      ctx.lineTo(lastX, lastY);
     }
   }
   frameCount++;
